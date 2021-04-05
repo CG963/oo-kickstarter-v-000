d0dc9953550701096a5ce2c446a0fc8a4b4f510f
Christopher Garcia <christopher.garcia08@gmail.com>
2021-04-05 06:20:27 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,9 @@
 Class Backer
-  attr_reader :name 
-  
-  @backed_projects = []
+  attr_reader :name, :backed_projects 
   
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
 end
