77be67529ae9bc95abe7eff695de2fcb63ce9bb8
Christopher Garcia <christopher.garcia08@gmail.com>
2021-04-05 06:38:52 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,14 @@
-Class Project
-  attr_accessor :funded, :backers 
-  
-  def initialize(funded)
-    @funded = funded
+class Project
+  attr_reader :title, :backers
+
+  def initialize(title)
+    @title = title
     @backers = []
   end
 
   def add_backer(backer)
-    @backers << backer
+    backers << backer
     backer.backed_projects << self
   end
-  
+
 end
